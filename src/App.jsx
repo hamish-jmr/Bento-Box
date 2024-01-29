f77cf381bac579bc3d634df865352c4bec10e66e
Hamish Roberts <hamish.jmr@gmail.com>
2024-01-30 10:51:28 +1300

    Set up bento box base

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-6xl'>
-        Vite X TailwindCSS Template
-      </h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-slate-400 py-5 rounded-lg">01</div>
+        <div className="bg-slate-200 py-5 rounded-lg">02</div>
+        <div className="bg-slate-400 py-5 rounded-lg">03</div>
+        <div className="col-span-2 bg-slate-200 py-5 rounded-lg">04</div>
+        <div className="bg-slate-400 py-5 rounded-lg">05</div>
+        <div className="bg-slate-200 py-5 rounded-lg">06</div>
+        <div className="col-span-2 bg-slate-400 py-5 rounded-lg">07</div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
