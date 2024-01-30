1ba44f2e61b650cb70f75eef333e8f87125519ea
Hamish Roberts <hamish.jmr@gmail.com>
2024-01-30 17:10:02 +1300

    Added breakpoints using responsive utility variants

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="grid grid-cols-3 gap-7">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
       <div
-        className="py-5 h-60 rounded-lg font-mono bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
+        className="col-span-1 py-5 h-60 rounded-lg font-mono bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
         <p></p>
       </div>
-      <div className="col-span-2 py-5 h-60 rounded-lg font-sans font-light bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
+      <div className="col-span-1 lg:col-span-2 py-5 h-60 rounded-lg font-sans font-light bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
         <p></p>
       </div>
-      <div className="col-span-2 py-5 h-60 rounded-lg bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
-        <p></p>
-      </div>
-      <div className="py-5 rounded-lg h-60 bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
+      <div className="col-span-1 lg:col-span-2 py-5 h-60 rounded-lg bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
         <p></p>
       </div>
       <div className="py-5 rounded-lg h-60 bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
         <p></p>
       </div>
-      <div className="col-span-2 py-5 h-60 rounded-lg bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
+      <div className="py-5 rounded-lg h-60 bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
+        <p></p>
+      </div>
+      <div className="col-span-1 lg:col-span-2 py-5 h-60 rounded-lg bg-sky-100 hover:drop-shadow-2xl hover:cursor-pointer hover:-translate-y-1 hover:scale-105 duration-300">
         <p></p>
       </div>
     </div>
